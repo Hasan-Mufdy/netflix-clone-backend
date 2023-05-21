fc291560105fec2f3bcd8f34aa2fa760b3d7c07e
Hasan-Mufdy <h.mufdy@gmail.com>
2023-05-21 21:07:05 +0000

    added update and delete functions

--- schema.sql
+++ schema.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS favMovies (
     id SERIAL PRIMARY KEY,
     favMovieName VARCHAR(255),
     favMoviePosterPath VARCHAR(255),
-    comment VARCHAR(255)
+    comment VARCHAR(255),
+    releaseDate VARCHAR(255)
 );
